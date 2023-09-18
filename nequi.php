c3dc545948121ba0f2b7da32166ceff343b387e9
Alex Monroy <alexwebpuro@gmail.com>
2023-09-18 02:12:35 -0500

    Initial commit.

---
+++ nequi.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Nequi payment method
+ *
+ * @package           Nequi
+ * @author            Alexwebpuro
+ * @copyright         2023 Alex Monroy
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Nequi
+ * Plugin URI:        https://alexmonroy.com/nequi
+ * Description:       Metodo de pago para Nequi.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Alexander Monroy
+ * Author URI:        https://alexmonroy.com
+ * Text Domain:       nequi-payment
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://alexmonroy.com/nequi/
+ */
